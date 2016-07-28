9842d77e85631a4f7d037d3955cbee49350ae03f
vinokeerthi <dollkeerthi012@gmail.com>
2016-07-28 15:39:25 +0530

    oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	if(num!=0)
+	if(num%2==0)
+	printf("even");
+	else
+	printf("odd");
+	else
+	printf("Neither even nor odd");
+	return 0;	
+	
+}
