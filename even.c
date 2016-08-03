9850dc0ce74e255e095820639c1f199a89708b11
vinokeerthi <dollkeerthi012@gmail.com>
2016-08-03 22:14:07 +0530

    even.c

--- even.c
+++ even.c
@@ -3,17 +3,14 @@ int main()
 {
 	int i,n1,n2;
 	scanf("%d%d",&n1,&n2);
-	for(i=n1;i<=n2;i++)
+	for(i=n1+2;i<=n2;i++)
 	{
-		if(n1%2==0)
+		if(i%2==0)
 		{
-			printf("%d",n1);
-			n1++;
+		printf("%d\n",i);
+			
 		}
-		else
-		{
-			n1++;
-		}
+		
 	}
 	return 0;
 }
